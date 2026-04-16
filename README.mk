dbd02184d28750c2acf8e664052096e8ddc1bdd0
Felipe Luiz Fonseca <felipe.fonseca@prumoengenharia.com.br>
2026-04-16 11:05:46 -0300

    Atualizacoes 2026-04-16 11:05

---
+++ README.mk
@@ -0,0 +1,12 @@
+# Seminário — Teoria Institucional
+
+Disciplina: Teoria das Organizações  
+Professor: Prof. Dr. Adriano Olímpio Tonelli  
+
+Apresentadores:
+- Felipe Luiz Fonseca
+- Paulo Aragão Daldegan
+
+## Tema
+Disrupções Institucionais e a Filantropia de Empresas Multinacionais
+DOI: https://doi.org/10.1287/orsc.2021.1491
